ec7e9606e64c4c22a0128b3eb17c79c6bc2e6e2c
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-10-16 14:38:20 +0200

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,1 +1,35 @@
-for EFIX_PKG in "efix1.tar|pakiet1.rte" "efix2.tar|pakiet2.rte" "efix3.tar|pakiet3.rte"; do EFIX=${EFIX_PKG%%|*}; PKG=${EFIX_PKG##*|}; lslpp -L | grep -iq "${PKG%.rte}" && echo "$PKG już zainstalowany" && continue; echo "Pobieram $EFIX..."; perl -MHTTP::Tiny -e 'HTTP::Tiny->new->mirror("'"link/security/$EFIX"'","/tmp/$EFIX")'; echo "Rozpakowuję $EFIX..."; tar -xvf "/tmp/$EFIX" -C /tmp; cd "/tmp/${EFIX%.tar}"; echo "Instaluję $PKG..."; installp -acXYd . "$PKG"; echo "EMGR $PKG..."; perl -e '$pkg_upd="'"$PKG"'"; $_=qx{/usr/sbin/emgr -P}; /(^$pkg_upd\s+\S+\s+(\S+))/m and exec "/usr/sbin/emgr","-r","-L",$2'; cd /tmp; done
+DECLARE @TableName NVARCHAR(255)
+DECLARE @ColumnName NVARCHAR(255)
+DECLARE @SearchStr NVARCHAR(100)
+DECLARE @SQL NVARCHAR(MAX)
+
+SET @SearchStr = 'error'  -- to, czego szukasz
+SET @SQL = ''
+
+
+SELECT t.name AS TableName, c.name AS ColumnName
+INTO #ToSearch
+FROM sys.tables t
+JOIN sys.columns c ON t.object_id = c.object_id
+JOIN sys.types y ON c.user_type_id = y.user_type_id
+WHERE y.name IN ('char', 'nchar', 'varchar', 'nvarchar', 'text', 'ntext')
+
+
+DECLARE cur CURSOR FOR SELECT TableName, ColumnName FROM #ToSearch
+OPEN cur
+FETCH NEXT FROM cur INTO @TableName, @ColumnName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @SQL = @SQL + 
+        'IF EXISTS (SELECT 1 FROM [' + @TableName + '] WHERE [' + @ColumnName + '] LIKE ''%' + @SearchStr + '%'') 
+         PRINT ''Found in table: ' + @TableName + ', column: ' + @ColumnName + ''';' + CHAR(13)
+    FETCH NEXT FROM cur INTO @TableName, @ColumnName
+END
+
+CLOSE cur
+DEALLOCATE cur
+DROP TABLE #ToSearch
+
+
+EXEC sp_executesql @SQL
