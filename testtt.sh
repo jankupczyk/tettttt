8146c8f0d9ffb2633f29c11c0f397b54b3ba7caa
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-10-16 17:57:42 +0200

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,24 +1,23 @@
--- 1. Parametry
--- Sprawdź swoją wartość w postgresql.conf
-SHOW autovacuum_freeze_max_age;
+WITH params AS (
+    SELECT 200000000::numeric AS max_age
+)
 
--- Załóżmy wynik np. 200000000
-\set max_age 200000000
-
--- 2. Wiek XID w bazach i procent wykorzystania
+-- Wiek XID w bazach i procent do autovacuum
 SELECT 
-    datname,
-    age(datfrozenxid) AS oldest_xid_age,
-    round((age(datfrozenxid)::numeric / :max_age) * 100, 2) AS percent_of_max_age
-FROM pg_database
+    d.datname,
+    age(d.datfrozenxid) AS oldest_xid_age,
+    round((age(d.datfrozenxid)::numeric / p.max_age) * 100, 2) AS percent_of_max_age
+FROM pg_database d
+CROSS JOIN params p
 ORDER BY percent_of_max_age DESC;
 
--- 3. Wiek XID w tabelach (uwzględniając TOAST) i procent
+-- Wiek XID w tabelach (uwzględniając TOAST) i procent
 SELECT 
     c.oid::regclass AS table_name,
     greatest(age(c.relfrozenxid), age(t.relfrozenxid)) AS oldest_xid_age,
-    round((greatest(age(c.relfrozenxid), age(t.relfrozenxid))::numeric / :max_age) * 100, 2) AS percent_of_max_age
+    round((greatest(age(c.relfrozenxid), age(t.relfrozenxid))::numeric / p.max_age) * 100, 2) AS percent_of_max_age
 FROM pg_class c
 LEFT JOIN pg_class t ON c.reltoastrelid = t.oid
+CROSS JOIN params p
 WHERE c.relkind IN ('r','m')
 ORDER BY percent_of_max_age DESC;
