80022839be62d71b02e3a20bf829018517b4def9
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-10-16 17:52:06 +0200

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,35 +1,12 @@
-DECLARE @TableName NVARCHAR(255)
-DECLARE @ColumnName NVARCHAR(255)
-DECLARE @SearchStr NVARCHAR(100)
-DECLARE @SQL NVARCHAR(MAX)
+-- Wiek najstarszych wierszy w bazie
+SELECT datname, age(datfrozenxid) AS oldest_xid_age FROM pg_database;
 
-SET @SearchStr = 'error'  -- to, czego szukasz
-SET @SQL = ''
-
-
-SELECT t.name AS TableName, c.name AS ColumnName
-INTO #ToSearch
-FROM sys.tables t
-JOIN sys.columns c ON t.object_id = c.object_id
-JOIN sys.types y ON c.user_type_id = y.user_type_id
-WHERE y.name IN ('char', 'nchar', 'varchar', 'nvarchar', 'text', 'ntext')
+-- Wiek najstarszych wierszy w tabeli
+SELECT c.oid::regclass AS table_name,
+       greatest(age(c.relfrozenxid), age(t.relfrozenxid)) AS oldest_xid_age
+FROM pg_class c
+LEFT JOIN pg_class t ON c.reltoastrelid = t.oid
+WHERE c.relkind IN ('r','m');
 
 
-DECLARE cur CURSOR FOR SELECT TableName, ColumnName FROM #ToSearch
-OPEN cur
-FETCH NEXT FROM cur INTO @TableName, @ColumnName
-
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    SET @SQL = @SQL + 
-        'IF EXISTS (SELECT 1 FROM [' + @TableName + '] WHERE [' + @ColumnName + '] LIKE ''%' + @SearchStr + '%'') 
-         PRINT ''Found in table: ' + @TableName + ', column: ' + @ColumnName + ''';' + CHAR(13)
-    FETCH NEXT FROM cur INTO @TableName, @ColumnName
-END
-
-CLOSE cur
-DEALLOCATE cur
-DROP TABLE #ToSearch
-
-
-EXEC sp_executesql @SQL
+VACUUM (FREEZE) big_table;
