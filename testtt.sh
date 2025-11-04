641c96ccee19ec4f8f5b34d97a76b65d4d19fe47
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-11-04 09:54:36 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -6,3 +6,7 @@ export PATH=$TUXDIR/bin:$PATH
 export LD_LIBRARY_PATH=$TUXDIR/lib:$LD_LIBRARY_PATH
 
 exec /usr/bin/perl /usr/local/zabbix/scripts/tuxedo_lld.pl
+$ENV{'TUXDIR'} = '/app/tuxedo';
+$ENV{'TUXCONFIG'} = '/app/wpr/tuxedo/tuxconfig';
+$ENV{'PATH'} = "$ENV{'TUXDIR'}/bin:$ENV{'PATH'}";
+$ENV{'LD_LIBRARY_PATH'} = "$ENV{'TUXDIR'}/lib:$ENV{'LD_LIBRARY_PATH'}";
