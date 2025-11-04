28d4a209497538ef24b8d58c0d6e0469f72a6fa7
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-11-04 09:39:57 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,51 +1,8 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use JSON;
-use IPC::Open2;
-use Tie::IxHash;
+#!/bin/bash
 
-my $tmadmin = '/path/to/tmadmin';
-my $debug   = 0;
+export TUXDIR=/app/tuxedo
+export TUXCONFIG=/app/wpr/tuxedo/tuxconfig
+export PATH=$TUXDIR/bin:$PATH
+export LD_LIBRARY_PATH=$TUXDIR/lib:$LD_LIBRARY_PATH
 
-
-my $pid = open2(my $out, my $in, "$tmadmin -r 2>/dev/null")
-    or die "Nie mogę uruchomić tmadmin: $!";
-
-print $in "pq\n";
-close $in;
-
-my %queues;
-while (my $line = <$out>) {
-    chomp $line;
-    $line =~ s/^\s+|\s+$//g;
-    next if $line eq '' or $line =~ /^Prog|^-{3,}/i;
-
-    my @cols = split(/\s+/, $line);
-    next unless scalar @cols >= 5;
-
-    my ($prog, $queued) = ($cols[0], $cols[4]);
-    $queued = 0 if ($queued !~ /^\d+$/);
-
-    $queues{$prog} += $queued;
-    print "[DEBUG] $prog -> $queued\n" if $debug;
-}
-close $out;
-
-
-my @data;
-foreach my $prog (sort keys %queues) {
-    tie my %ordered, 'Tie::IxHash';
-    %ordered = (
-        '{#PROGNAME}' => $prog,
-        'QUEUED'      => $queues{$prog},
-    );
-    push @data, \%ordered;
-}
-
-my %json_out = ( data => \@data );
-
-
-print to_json(\%json_out, { utf8 => 1, pretty => 1 });
-
-exit 0;
+exec /usr/bin/perl /usr/local/zabbix/scripts/tuxedo_lld.pl
