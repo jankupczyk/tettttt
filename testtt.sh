ab24070bd531b9c4332aa31c9684a3f4f397adf1
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-11-05 09:31:01 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,91 +1,17 @@
-#!/bin/bash
-
-export TUXDIR=/app/tuxedo
-export TUXCONFIG=/app/wpr/tuxedo/tuxconfig
-export PATH=$TUXDIR/bin:$PATH
-export LD_LIBRARY_PATH=$TUXDIR/lib:$LD_LIBRARY_PATH
-
-exec /usr/bin/perl /usr/local/zabbix/scripts/tuxedo_lld.pl
-$ENV{'TUXDIR'} = '/app/tuxedo';
-$ENV{'TUXCONFIG'} = '/app/wpr/tuxedo/tuxconfig';
-$ENV{'PATH'} = "$ENV{'TUXDIR'}/bin:$ENV{'PATH'}";
-$ENV{'LD_LIBRARY_PATH'} = "$ENV{'TUXDIR'}/lib:$ENV{'LD_LIBRARY_PATH'}";
-
-
+SELECT
+    i.idxname,
+    i.tabname,
+    DECODE(i.idxtype, 'U', 'UNIQUE', 'D', 'NON-UNIQUE', i.idxtype) AS index_type,
+    LIST(c.colname) AS columns
+FROM sysindexes i
+JOIN syscolumns c
+  ON c.tabid = i.tabid
+ AND c.colno IN (i.part1, i.part2, i.part3, i.part4)
+WHERE i.tabname = 'test'
+GROUP BY 1,2,3
+ORDER BY 1;
 
 
-
-
-my $env_file = '/app/tuxedo/tux.env';
-my $tmadmin  = '/app/tuxedo/bin/tmadmin';
-my $debug    = 0;
-
-
-if (-f $env_file) {
-    open my $fh, '<', $env_file or die "Nie mogę otworzyć $env_file: $!";
-    while (my $line = <$fh>) {
-        next if $line =~ /^\s*#/;
-        next unless $line =~ /=/;
-        $line =~ s/;\s*export.*$//;
-        $line =~ s/export\s+//;
-        chomp $line;
-
-        if ($line =~ /^(\w+)=(.*)$/) {
-            my ($key, $val) = ($1, $2);
-            $val =~ s/^"|"$//g;
-            $ENV{$key} = $val;
-            print "[DEBUG] ENV $key=$val\n" if $debug;
-        }
-    }
-    close $fh;
-} else {
-    die "Brak pliku środowiska: $env_file\n";
-}
-
-
-$.data[?(@["{#PROGNAME}"]=="{#PROGNAME}")].QUEUED.first()
-
-$.data[?(@["{#PROGNAME}"]=="{#PROGNAME}")].QUEUED.first() || 0
-
-
-var obj = JSON.parse(value);
-var prog = "{#PROGNAME}";
-for (var i=0; i<obj.data.length; i++) {
-  if (obj.data[i]["{#PROGNAME}"] === prog) {
-    return obj.data[i].QUEUED;
-  }
-}
-return null;
-
-
-
-
-var obj = JSON.parse(value);
-var prog = "{#PROGNAME}";
-var found = [];
-for (var i = 0; i < obj.data.length; i++) {
-    found.push(obj.data[i]["{#PROGNAME}"]);
-    if (obj.data[i]["{#PROGNAME}"] === prog) {
-        return obj.data[i].QUEUED;
-    }
-}
-return "Szukano " + prog + ", znaleziono: " + found.join(",");
-
-
-
-
-
-
-
-var obj = JSON.parse(value);
-var prog = "{#PROGNAME}".trim();
-
-for (var i = 0; i < obj.data.length; i++) {
-    if (String(obj.data[i]["{#PROGNAME}"]).trim() === prog) {
-        return obj.data[i].QUEUED;
-    }
-}
-return 0;
-
-
-
+SELECT constrname, constrtype
+FROM sysconstraints
+WHERE tabid = (SELECT tabid FROM systables WHERE tabname = 'test');
