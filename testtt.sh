0268110528c47bf4c121c114464149401546a89c
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-11-08 12:35:21 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,17 +1,1 @@
-SELECT
-    i.idxname,
-    i.tabname,
-    DECODE(i.idxtype, 'U', 'UNIQUE', 'D', 'NON-UNIQUE', i.idxtype) AS index_type,
-    LIST(c.colname) AS columns
-FROM sysindexes i
-JOIN syscolumns c
-  ON c.tabid = i.tabid
- AND c.colno IN (i.part1, i.part2, i.part3, i.part4)
-WHERE i.tabname = 'test'
-GROUP BY 1,2,3
-ORDER BY 1;
-
-
-SELECT constrname, constrtype
-FROM sysconstraints
-WHERE tabid = (SELECT tabid FROM systables WHERE tabname = 'test');
+awk '/Thread-/{if(t){print t}; t=0} /at /{t++} END{print t}' /tmp/tomcat_jstack_YYYY-MM-DD_HH:MM:SS.txt | sort -nr | head
