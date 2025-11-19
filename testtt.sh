4a4278426fb8f5158c1bb6961a18066029cadbe6
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-11-19 15:22:13 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,1 +1,1 @@
-lsuser -c -a name gecos ALL | sed '1d' | awk -F: '$1!="root"{split($NF,a,"="); print $1 " : " a[2]}' > users_gecos.txt
+lsuser -c ALL | sed '1d' | awk -F: '$1!="root"{g=""; for(i=1;i<=NF;i++) if($i~/^gecos=/){sub(/^gecos=/,"",$i); g=$i} print $1 " : " g}'
