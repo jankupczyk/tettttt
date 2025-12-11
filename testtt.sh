f4f652c074122826a0de358a0a67c765dd452440
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-12-11 14:18:15 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,13 +1,1 @@
-system("grep -v -E '^(Name|----|Length|=+)\$' raport.txt | grep -v '^\\s*\$' > raport_tmp && mv raport_tmp raport.txt");
-
-
-# Ścieżka do pliku wynikowego
-my $raport = $output;
-my $tmp = "$raport.tmp";
-
-# Usuń nagłówki, separatory i puste linie przy pomocy sed
-system("sed '/^\\s*$/d; /^Name\$/d; /^----\$/d; /^Length\$/d; /^=\\+\$/d' $raport > $tmp");
-
-# Nadpisanie oryginalnego pliku czystym raportem
-rename $tmp, $raport or die "Nie mogę nadpisać pliku $raport: $!";
-
+awk '{ for(i=1;i<=NF;i++) if($i !~ /^(Name|----|Length|=+)$/) print $i }' raport.txt > raport_czysty.txt
