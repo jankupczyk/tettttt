3a7cf0d030d5e9b79ecff04a6b008a6b095cb57a
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-12-11 13:58:47 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -3,12 +3,12 @@ open my $OUT, ">", $output or die "Nie mogę otworzyć $output: $!";
 foreach my $zip (@zip_list) {
     foreach my $line (`unzip -l "$zip" 2>/dev/null`) {
         chomp $line;
-        next if $line =~ /^\s*$/;         # pomiń puste linie
-        next if $line =~ /^(Length|----|Name)/; # pomiń nagłówki/separatory
-        my @cols = split ' ', $line;
-        next unless @cols >= 4;
-        # ostatnia kolumna to nazwa pliku
-        print $OUT "$cols[-1]\n";
+        $line =~ s/^\s+|\s+$//g;      # usuń białe znaki na początku i końcu
+        next unless $line;             # pomiń puste linie
+        # Pomijamy linie nagłówków i separatorów
+        next if $line =~ /^(Length|Name|----|=+)$/i;
+        # Wszystko inne traktujemy jako nazwę pliku
+        print $OUT "$line\n";
     }
 }
 
