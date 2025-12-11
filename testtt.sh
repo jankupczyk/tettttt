9c1d15c2260a25fbc4498250b8f809c49d4dccf4
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-12-11 14:05:50 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,15 +1,1 @@
-open my $OUT, ">", $output or die "Nie mogę otworzyć $output: $!";
-
-foreach my $zip (@zip_list) {
-    foreach my $line (`unzip -l "$zip" 2>/dev/null`) {
-        chomp $line;
-        $line =~ s/^\s+|\s+$//g;      # usuń białe znaki na początku i końcu
-        next unless $line;             # pomiń puste linie
-        # Pomijamy linie nagłówków i separatorów
-        next if $line =~ /^(Length|Name|----|=+)$/i;
-        # Wszystko inne traktujemy jako nazwę pliku
-        print $OUT "$line\n";
-    }
-}
-
-close($OUT);
+system("grep -v -E '^(Name|----|Length|=+)\$' raport.txt | grep -v '^\\s*\$' > raport_tmp && mv raport_tmp raport.txt");
