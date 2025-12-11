40279bcc8ae1da3bd3785e56edeb80a1c67d1ef4
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-12-11 13:52:59 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,97 +1,15 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use Getopt::Long;
-use POSIX qw(mktime);
-
-# Parametry
-my ($directory, $listfile, $output, $period, $since, $until);
-GetOptions(
-    "dir=s"    => \$directory,
-    "list=s"   => \$listfile,
-    "out=s"    => \$output,
-    "period=i" => \$period,
-    "since=s"  => \$since,
-    "until=s"  => \$until,
-);
-
-die "Użycie:\n  --dir <katalog> [--list plik.txt] --out wynik.txt [--period min | --since ... [--until ...]]\n"
-    unless $directory && $output;
-
-
-my @zip_list;
-
-if ($listfile) {
-    open my $LF, "<", $listfile or die "Nie mogę otworzyć $listfile: $!";
-    while (<$LF>) {
-        chomp;
-        next unless $_;
-        my $zip_path = -f $_ ? $_ : "$directory/$_";
-        push @zip_list, $zip_path if -f $zip_path;
-    }
-    close $LF;
-} else {
-    my $time_from;
-    my $time_to = time();
-
-    if ($period) {
-        $time_from = time() - ($period * 60);
-    } elsif ($since) {
-        $since =~ s/[-: ]/ /g;
-        my @t = split /\s+/, $since;
-        $time_from = mktime(0, $t[4], $t[3], $t[2], $t[1]-1, $t[0]-1900);
-    } else {
-        $time_from = 0;
-    }
-
-    if ($until) {
-        $until =~ s/[-: ]/ /g;
-        my @u = split /\s+/, $until;
-        $time_to = mktime(0, $u[4], $u[3], $u[2], $u[1]-1, $u[0]-1900);
-    }
-
-    opendir my $D, $directory or die "Nie mogę otworzyć katalogu $directory: $!";
-    foreach my $f (grep { /\.zip$/i } readdir($D)) {
-        my $full = "$directory/$f";
-        my $mtime = (stat($full))[9];
-        next if $mtime < $time_from || $mtime > $time_to;
-        push @zip_list, $full;
-    }
-    closedir $D;
-}
 
 open my $OUT, ">", $output or die "Nie mogę otworzyć $output: $!";
+
 foreach my $zip (@zip_list) {
+    my $in_files_section = 0;
     foreach my $line (`unzip -l "$zip" 2>/dev/null`) {
         chomp $line;
-        next if $line =~ /^(Length|=|$)/;
-        my @cols = split ' ', $line;
-        next unless defined $cols[3];
-        print $OUT "$cols[3]\n";
+        $in_files_section = 1 if $line =~ /^Name$/;
+        next unless $in_files_section;
+        next if $line =~ /^(Name|----|\s*)$/;
+        print $OUT "$line\n";
     }
 }
-close($OUT);
 
-print "Raport gotowy: $output\n";
-
-
-
-
-
-
-
-
-
-open my $OUT, ">", $output or die "Nie mogę otworzyć $output: $!";
-foreach my $zip (@zip_list) {
-
-    foreach my $line (`unzip -l "$zip" 2>/dev/null`) {
-        chomp $line;
-
-        my @cols = split ' ', $line;
-        next unless @cols >= 4;
-
-        print $OUT "$cols[-1]\n";
-    }
-}
 close($OUT);
