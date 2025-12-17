9ea0d6fc18cc0312ce542740c75c3351f76a3e7a
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-12-17 20:03:40 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,16 +1,30 @@
-@SpringBootApplication
-@RestController
-public class HeaderTestApplication {
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.stream.Collectors;
 
-    public static void main(String[] args) {
-        SpringApplication.run(HeaderTestApplication.class, args);
-    }
+public class HeaderServer {
 
-    @GetMapping("/headers")
-    public Map<String, String> headers(HttpServletRequest request) {
-        Map<String, String> map = new HashMap<>();
-        Collections.list(request.getHeaderNames())
-                .forEach(h -> map.put(h, request.getHeader(h)));
-        return map;
+    public static void main(String[] args) throws Exception {
+        int port = 8213;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/headers", (HttpExchange exchange) -> {
+
+            String headers = exchange.getRequestHeaders().entrySet().stream()
+                .map(e -> e.getKey() + " = " + e.getValue())
+                .collect(Collectors.joining("\n"));
+
+            String response = "HEADERS RECEIVED:\n\n" + headers;
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.start();
+        System.out.println("Header test server running on port " + port);
     }
 }
