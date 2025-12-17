7eb665e29eb816fa89473d183b3408126f8e414e
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-12-17 20:28:45 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -2,12 +2,15 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class HeaderHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print("=== HEADERS RECEIVED ===")
+        for k, v in self.headers.items():
+            print(f"{k} = {v}")
+        print("=======================")
+
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-
-        headers_received = "\n".join(f"{k} = {v}" for k, v in self.headers.items())
-        self.wfile.write(headers_received.encode())
+        self.wfile.write("OK\n".encode())
 
 port = 8213
 server = HTTPServer(('0.0.0.0', port), HeaderHandler)
