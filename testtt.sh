517fe148a19b80a609f3c6d499c389da3b650093
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2025-12-20 12:40:28 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,18 +1,1 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-class HeaderHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print("=== HEADERS RECEIVED ===")
-        for k, v in self.headers.items():
-            print(f"{k} = {v}")
-        print("=======================")
-
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write("OK\n".encode())
-
-port = 8213
-server = HTTPServer(('0.0.0.0', port), HeaderHandler)
-print(f"Header test server running on port {port}")
-server.serve_forever()
+:%s|/opt/zabbix_agent_3/scripts/|/opt/zabbix_agent_7/scripts/|g
