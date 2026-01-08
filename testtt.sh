3747a30d2f98ad26cd096da353df304e6dab5d8f
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-01-08 15:03:22 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,15 +1,8 @@
-SELECT
-    st.tabname   AS child_table,
-    sc.constrname,
-    sc.constrtype
-FROM sysconstraints sc
-JOIN systables st ON sc.tabid = st.tabid
-WHERE sc.constrtype = 'R'
-AND sc.primary = (
-    SELECT constrid
-    FROM sysconstraints
-    WHERE constrtype = 'P'
-    AND tabid = (
-        SELECT tabid FROM systables WHERE tabname = 'temp_mail'
-    )
-);
+keytool -genkeypair -alias fqdnmojegoserwera -keyalg RSA -keysize 4096 -validity 365 -keystore fqdnmojegoserwera.jks -storetype JKS -dname "CN=fqdnmojegoserwera,O=organizacja moja,OU=test,L=poznan,ST=wielkopolskie,C=PL,EMAILADDRESS=mojmail@firma.pl" -ext san=dns:fqdnmojegoserwera,email:mojmail@firma.pl
+
+
+
+keytool -list -v -keystore fqdnmojegoserwera.jks -storetype JKS -alias fqdnmojegoserwera
+
+
+keytool -certreq -alias fqdnmojegoserwera -keystore fqdnmojegoserwera.jks -storetype JKS -file fqdnmojegoserwera.csr
