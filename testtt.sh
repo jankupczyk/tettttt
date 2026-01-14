863e40eee524764e4eea2aec5b448f3d5a3297a2
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-01-14 11:48:26 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,8 +1,35 @@
-keytool -genkeypair -alias fqdnmojegoserwera -keyalg RSA -keysize 4096 -validity 365 -keystore fqdnmojegoserwera.jks -storetype JKS -dname "CN=fqdnmojegoserwera,O=organizacja moja,OU=test,L=poznan,ST=wielkopolskie,C=PL,EMAILADDRESS=mojmail@firma.pl" -ext san=dns:fqdnmojegoserwera,email:mojmail@firma.pl
+<VirtualHost *:443>
+    ServerName localhost
+    DocumentRoot "D:/xampp/htdocs/DOCROOT/public"
+
+    SSLEngine on
+    SSLCertificateFile "D:/xampp/apache/conf/ssl.crt/server.crt"
+    SSLCertificateKeyFile "D:/xampp/apache/conf/ssl.key/server_nopass.key"
+    SSLCertificateChainFile "D:/xampp/apache/conf/ssl.crt/chain.crt"
+
+    SSLProtocol -all +TLSv1.2 +TLSv1.3
+    SSLCipherSuite TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256
+    SSLHonorCipherOrder on
+
+    LimitRequestBody 10485760
 
 
+    Header always set Strict-Transport-Security "max-age=31536000"
 
-keytool -list -v -keystore fqdnmojegoserwera.jks -storetype JKS -alias fqdnmojegoserwera
+    Header always set X-Content-Type-Options "nosniff"
+    Header always set X-Frame-Options "DENY"
+    Header always set X-XSS-Protection "1; mode=block"
+    Header always set Referrer-Policy "strict-origin-when-cross-origin"
 
+    <Directory "D:/xampp/htdocs/DOCROOT/public">
+        AllowOverride All
+        Require all granted
+        <LimitExcept GET POST>
+            Deny from all
+        </LimitExcept>
+    </Directory>
 
-keytool -certreq -alias fqdnmojegoserwera -keystore fqdnmojegoserwera.jks -storetype JKS -file fqdnmojegoserwera.csr
+    ErrorLog "logs/ssl_error.log"
+    CustomLog "logs/ssl_access.log" combined
+
+</VirtualHost>
