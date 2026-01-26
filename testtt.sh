9ae80346cfdfa1fb76fe5065c5c989c188839faa
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-01-26 11:43:28 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,23 +1,4 @@
-#!/bin/ksh
-SEARCH="TESTOWY123"
-DBNAME="moja_baza"
-
-dbaccess $DBNAME <<SQL | awk '{print $1,$2}' > columns.txt
-SELECT t.tabname, c.colname
-FROM systables t
-JOIN syscolumns c ON t.tabid = c.tabid
-WHERE c.coltype IN (0,13,17) -- CHAR, LVARCHAR, TEXT
-AND t.tabid >= 100;
-SQL
-
-# sprawdz każdą kolumnę
-while read table col
-do
-  echo "Sprawdzam $table.$col ..."
-  dbaccess $DBNAME <<SQL
-    SELECT "$table.$col" AS found_in
-    FROM $table
-    WHERE $col LIKE '%$SEARCH%'
-    FIRST 1;
-SQL
+for m in GET POST PUT DELETE PATCH OPTIONS; do
+  echo "== $m =="
+  curl -k -s -o /dev/null -w "%{http_code}\n" -X $m https://host/api
 done
