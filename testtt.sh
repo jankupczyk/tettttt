0e4e8edb04c94305d19a55ec2e612d855fa17e8f
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-01-27 10:36:16 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,4 +1,21 @@
-for m in GET POST PUT DELETE PATCH OPTIONS; do
-  echo "== $m =="
-  curl -k -s -o /dev/null -w "%{http_code}\n" -X $m https://host/api
-done
+CREATE LOGIN [DOMAIN\user1] FROM WINDOWS;
+CREATE LOGIN [DOMAIN\user2] FROM WINDOWS;
+
+USE TwojaBaza;
+GO
+
+CREATE USER [DOMAIN\user1] FOR LOGIN [DOMAIN\user1];
+CREATE USER [DOMAIN\user2] FOR LOGIN [DOMAIN\user2];
+
+
+ALTER ROLE db_datareader ADD MEMBER [DOMAIN\user1];
+ALTER ROLE db_datareader ADD MEMBER [DOMAIN\user2];
+
+USE TwojaBaza;
+SELECT 
+    u.name AS user_name,
+    r.name AS role_name
+FROM sys.database_role_members drm
+JOIN sys.database_principals r ON drm.role_principal_id = r.principal_id
+JOIN sys.database_principals u ON drm.member_principal_id = u.principal_id
+WHERE u.name IN ('DOMAIN\user1', 'DOMAIN\user2');
