c2ad020d206a9e62ab3ccdd371257b4872f7c126
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-01-27 18:54:29 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,3 +1,14 @@
-for /R %f in (*) do (
-  findstr /I /R "127\.0\.0\.1|192\.168\.0\.1|10\.0\.0\.1" "%f" >nul 2>&1 && echo %f
-)
+@echo off
+set DATE=%DATE:~-4%-%DATE:~3,2%-%DATE:~0,2%
+set BACKUP_DIR=C:\backup
+set DB_USER=root
+set MYSQLDUMP_PATH=C:\xampp\mysql\bin\mysqldump.exe
+
+if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
+
+"%MYSQLDUMP_PATH%" -u %DB_USER% --all-databases --port=3306 > "%BACKUP_DIR%\all_databases_%DATE%.sql"
+
+powershell Compress-Archive "%BACKUP_DIR%\all_databases_%DATE%.sql" "%BACKUP_DIR%\all_databases_%DATE%.zip" -Force
+
+del "%BACKUP_DIR%\all_databases_%DATE%.sql"
+pause
