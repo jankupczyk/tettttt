45a0bcc25b082864b02dc78ca49e15462d39617c
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-01-27 16:50:45 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,21 +1,4 @@
-CREATE LOGIN [DOMAIN\user1] FROM WINDOWS;
-CREATE LOGIN [DOMAIN\user2] FROM WINDOWS;
-
-USE TwojaBaza;
-GO
-
-CREATE USER [DOMAIN\user1] FOR LOGIN [DOMAIN\user1];
-CREATE USER [DOMAIN\user2] FOR LOGIN [DOMAIN\user2];
-
-
-ALTER ROLE db_datareader ADD MEMBER [DOMAIN\user1];
-ALTER ROLE db_datareader ADD MEMBER [DOMAIN\user2];
-
-USE TwojaBaza;
-SELECT 
-    u.name AS user_name,
-    r.name AS role_name
-FROM sys.database_role_members drm
-JOIN sys.database_principals r ON drm.role_principal_id = r.principal_id
-JOIN sys.database_principals u ON drm.member_principal_id = u.principal_id
-WHERE u.name IN ('DOMAIN\user1', 'DOMAIN\user2');
+$patterns = '127\.0\.0\.1','192\.168\.0\.1','10\.0\.0\.1'
+Get-ChildItem -Recurse -File |
+  Select-String -Pattern $patterns -CaseSensitive:$false |
+  Select-Object -Unique Path
