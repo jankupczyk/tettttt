8c406b4c150fb7662ca8248d002175a1b5f7dc88
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-02-18 17:01:48 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,2 +1,5 @@
 zip 2024.zip $(find . -maxdepth 1 -type f -newermt 2024-01-01 ! -newermt 2025-01-01)
 zip 2025.zip $(find . -maxdepth 1 -type f -newermt 2025-01-01 ! -newermt 2026-01-01)
+
+
+for f in /katalog_zrodlowy/*; do [ -f "$f" ] && echo "INFO:: FILE - $(basename "$f")" && mv "$f" /katalog_docelowy/ && sleep 15; done
