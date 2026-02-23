15e57704c0df2c088740d89ff28dd280fe0a6b0e
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-02-23 19:00:27 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-
-for val in FOLDER1 FOLDER2 FOLDER3
-do
-    ls -1t /test/folder/$val/OUT 2>/dev/null | head -10
-done
+spring.datasource.url=jdbc:informix-sqli://HOST:PORT/DBNAME:INFORMIXSERVER=SERVERNAME
+spring.datasource.username=USER
+spring.datasource.password=PASSWORD
+spring.datasource.driver-class-name=com.informix.jdbc.IfxDriver
+spring.jpa.database-platform=org.hibernate.community.dialect.InformixDialect
+spring.jpa.hibernate.ddl-auto=none
+spring.jpa.show-sql=true
+spring.jpa.properties.hibernate.format_sql=true
