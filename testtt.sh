e1930a6a6aea3933c3c10b685ce8bc0547bb1463
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-02-25 17:45:17 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
+#PROCENT LOGOW NIEZBACKPOWANYCH
+onstat -l | awk '
+BEGIN{i=0; bad=0}
+/^[0-9]/ {
+    if ($3 !~ /C/) {
+        i++
+        if ($3 !~ /B/) bad++
+    }
+}
+END{
+    if (i>0) printf "%.2f\n", 100*bad/i
+    else print "0.00"
+}'
 
-if ! ifconfig en2 | grep -q "UP"; then
-    echo "CRIT: en2 DOWN"
-    exit 1
-fi
-
-
-PENDING=$(onstat -l | awk '
+#ILOSC LOGOW OCZEKUJACYCH NA BACKUP
+onstat -l | awk '
+BEGIN{bad=0}
 /^[0-9]/ {
     if ($3 ~ /U/ && $3 !~ /B/ && $3 !~ /C/)
         bad++
 }
-END { print bad+0 }')
-
-if [ "$PENDING" -gt 0 ]; then
-    echo "CRIT: $PENDING logs waiting for backup"
-    exit 2
-fi
-
-echo "OK"
-exit 0
+END { print bad+0 }'
