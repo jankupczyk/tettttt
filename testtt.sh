860b1415c5b63ef80f60969833a68674b5bc1d92
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-02-27 11:14:47 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -3,3 +3,10 @@ find /dane -mindepth 2 -maxdepth 2 -type d -name OUT -exec sh -c '
     count=$(find "$1" -maxdepth 1 -type f | wc -l)
     echo "$nik $count"
 ' _ {} \;
+
+
+find /dane -mindepth 2 -maxdepth 2 -type d -name OUT | while read outdir; do 
+    nik=$(basename "$(dirname "$outdir")"); 
+    count=$(find "$outdir" -maxdepth 1 -type f | wc -l); 
+    echo "$nik $count"; 
+done
