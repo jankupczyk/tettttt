6b8dc7d537f3aec3c1d189fdd3fb9d06d0238c47
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-02 20:00:14 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,1 +1,28 @@
-mkdir -p /dane/123456/OUT_BAC && find /dane/123456/OUT -maxdepth 1 -type f -newermt "2000-01-01" ! -newermt "2025-01-01" -exec mv {} /dane/123456/OUT_BAC/ \;
+# C:\Windows\System32\inetsrv\backup\IIS_Compliance_2026-03-02
+Stworzyc backup
+cd C:\Windows\System32\inetsrv
+
+.\appcmd add backup "IIS_Compliance_2026-03-02"
+
+
+
+# restore
+cd C:\Windows\System32\inetsrv
+
+.\appcmd restore backup "IIS_Compliance_2026-03-02"
+
+iisreset
+
+
+##########################################################################################################
+
+# 
+Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name logFormat -value "W3C"
+
+Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name enabled -value True
+
+Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name logExtFileFlags -value "Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,HttpSubStatus,Referer,Host,Cookie"
+
+
+
+Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.web/httpCookies" -name httpOnlyCookies -value True
