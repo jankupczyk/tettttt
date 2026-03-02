65b3c629ad68c9c57b5c7e94e56af9e1525d81aa
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-02 20:57:29 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,13 +1,2 @@
-Import-Module WebAdministration
-
-$logDefaults = Get-WebConfiguration -Filter "system.applicationHost/sites/siteDefaults/logFile" -PSPath IIS:\
-
-$currentFields = $logDefaults.logExtFileFlags
-
-if ($currentFields -notmatch "X-Forwarded-For") {
-    $newFields = $currentFields + ",X-Forwarded-For"
-    Set-WebConfigurationProperty -Filter "system.applicationHost/sites/siteDefaults/logFile" -PSPath IIS:\ -Name "logExtFileFlags" -Value $newFields
-    Write-Host "INFOO:: Added X-Forwarded-For in the siteDefaults logFieldName: $newFields"
-} else {
-    Write-Host "WARN:: X-Forwarded-For already exists in the siteDefaults logFieldName"
-}
+Set-WebConfigurationProperty -Filter "system.web/hosting/deployment" -PSPath IIS:\ -Name "retail" -Value $true
+Write-Host "Ustawiono deployment retail=true"
