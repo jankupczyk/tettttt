d844a4692cc970e4469f4913e1a711a50f561748
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-02 20:47:39 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,50 +1,13 @@
-# C:\Windows\System32\inetsrv\backup\IIS_Compliance_2026-03-02
-Stworzyc backup
-cd C:\Windows\System32\inetsrv
-
-.\appcmd add backup "IIS_Compliance_2026-03-02"
-
-
-
-# restore
-cd C:\Windows\System32\inetsrv
-
-.\appcmd restore backup "IIS_Compliance_2026-03-02"
-
-iisreset
-
-
-##########################################################################################################
-
-# 
-Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name logFormat -value "W3C"
-
-Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name enabled -value True
-
-Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name logExtFileFlags -value "Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,HttpSubStatus,Referer,Host,Cookie"
-
-
-
-Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.web/httpCookies" -name httpOnlyCookies -value True
-
-
-
-$newLogPath = "D:\IISLogs"
 Import-Module WebAdministration
-$sites = Get-ChildItem IIS:\Sites
 
-foreach ($site in $sites) {
+$logDefaults = Get-WebConfiguration -Filter "system.applicationHost/sites/siteDefaults/logFile" -PSPath IIS:\
 
-    Set-ItemProperty "IIS:\Sites\$($site.Name)" -Name logFile.directory -Value $newLogPath
-    Write-Host "Zmieniono folder logów dla strony '$($site.Name)' na $newLogPath"
+$currentFields = $logDefaults.logExtFileFlags
 
-    Set-WebConfigurationProperty -Filter "system.webServer/security/requestFiltering" -PSPath "IIS:\Sites\$($site.Name)" -Name "allowHighBitCharacters" -Value $false
-    Write-Host "Ustawiono allowHighBitCharacters = False dla strony '$($site.Name)'"
-
-    Set-WebConfigurationProperty -Filter "system.webServer/security/authentication/anonymousAuthentication" -PSPath "IIS:\Sites\$($site.Name)" -Name "enabled" -Value $false
-    Write-Host "Ustawiono anonymousAuthentication = False dla strony '$($site.Name)'"
+if ($currentFields -notmatch "X-Forwarded-For") {
+    $newFields = $currentFields + ",X-Forwarded-For"
+    Set-WebConfigurationProperty -Filter "system.applicationHost/sites/siteDefaults/logFile" -PSPath IIS:\ -Name "logExtFileFlags" -Value $newFields
+    Write-Host "INFOO:: Added X-Forwarded-For in the siteDefaults logFieldName: $newFields"
+} else {
+    Write-Host "WARN:: X-Forwarded-For already exists in the siteDefaults logFieldName"
 }
-
-
-Set-WebConfigurationProperty -Filter "system.applicationHost/sites/siteDefaults/logFile" -PSPath IIS:\ -Name "logExtFileFlags" -Value "Date,Time,ClientIP,Method,UriStem,UserAgent"
-Write-Host "Zmieniono siteDefaults logFieldName na: Date,Time,ClientIP,Method,UriStem,UserAgent"
