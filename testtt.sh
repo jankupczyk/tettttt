27300d5661a66279a568687469cc7fb194c37162
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-11 15:48:38 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,1 +1,16 @@
- 
+SELECT i.idxname, d.dbsname
+FROM sysindexes i
+JOIN systables t ON i.tabid = t.tabid
+JOIN sysdbspaces d ON i.dbsnum = d.dbsnum
+WHERE t.tabname = 'transaction'
+AND t.owner = 'powi';
+
+
+
+######################################################
+
+SELECT t.tabname, d.dbsname
+FROM systables t
+JOIN sysdbspaces d ON t.dbsnum = d.dbsnum
+WHERE t.tabname = 'transaction'
+AND t.owner = 'powi';
