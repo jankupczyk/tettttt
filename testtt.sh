ad728e75c3ff9542f586b2d8aa35676fb3895f28
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-23 18:17:46 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,3 +1,5 @@
-for f in *_*.xml; do
-    mv "$f" "$(echo "$f" | sed -E 's/_[0-9]{8}\.xml$/.xml/')"
+for f in *_*.xml
+do
+    new=$(echo "$f" | sed 's/_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\.xml$/.xml/')
+    mv "$f" "$new"
 done
