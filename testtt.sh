141134efd0333a4d57403ecb560ba1ffa4a1e841
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-23 12:15:53 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-output="prawa_jdk.sh"
-echo "#!/bin/bash" > $output
-echo "set -e" >> $output
-
-find . -exec stat -c '%a %U %G %n' {} \; 2>/dev/null | while read mode user group file; do
-    echo "chown $user:$group \"$file\"" >> $output
-    echo "chmod $mode \"$file\"" >> $output
-done
-
-chmod +x $output
+=SUMA(JEŻELI(CZĘSTOŚĆ(MATCH(A2:A603;A2:A603;0);MATCH(A2:A603;A2:A603;0))>0;1))
