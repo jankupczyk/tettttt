7fec4cc9854d221417bce9a97cce069ed6e636fa
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-23 13:31:58 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,1 +1,4 @@
-grep -Eo "<XchgRate[^>]*></XchgRate>|<XchgRate[^>]*/>" plik.xml | wc -l
+for f in *.xml; do
+  cp "$f" "$f.bak"
+  sed -Ei 's|<XchgRate>[[:space:]]*</XchgRate>|<XchgRate>1</XchgRate>|g; s|<XchgRate[[:space:]]*/>|<XchgRate>1</XchgRate>|g' "$f"
+done
