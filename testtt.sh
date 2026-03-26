8ebc326035f90da152476ca1fa6c2eecfaeb1253
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-03-26 16:12:36 +0100

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,33 +1,3 @@
-#!/usr/bin/perl
-
-use strict;
-use warnings;
-
-open my $onstat, "-|", "$ENV{INFORMIXDIR}/bin/onstat -u"
-    or die "Cannot run onstat";
-
-my ($active, $total);
-
-while (my $line = <$onstat>) {
-    if ($line =~ /(\d+)\D+(\d+)\D+(\d+)/) {
-
-        my ($a, $t, $m) = ($1, $2, $3);
-        next if $a > $t;          # active nie może być > total
-        next if $t == 0;          # total musi mieć sens
-        next if $m < $a;          # max >= active
-
-        $active = $a;
-        $total  = $t;
-        last;
-    }
-}
-
-close $onstat;
-
-unless (defined $active && defined $total && $total > 0) {
-    die "Cannot parse onstat -u output";
-}
-
-my $usage = ($active / $total) * 100;
-
-printf "%.2f\n", $usage;
+$ENV{'INFORMIXDIR'}    ||= '/opt/informix';   # ścieżka do Informix
+$ENV{'INFORMIXSERVER'} ||= 'informix';       # nazwa serwera
+$ENV{'PATH'} = "$ENV{INFORMIXDIR}/bin:$ENV{PATH}";
