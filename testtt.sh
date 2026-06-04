2cc9b6630c0f6b96ff871810138469220a6ae578
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-06-04 22:05:08 +0200

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,1 +1,24 @@
-perl -e 'use Digest::CRC qw(crc32); open(F,"plik.txt"); read(F,$_, -s F); print crc32($_), "\n";'
+awk '
+BEGIN { found=0; prev_broken=0; prev_line="" }
+{
+    line = $0
+    gsub(/[ \t]+$/, "", line)
+ 
+    last_char = substr(line, length(line), 1)
+ 
+    if (last_char != "|") {
+        printf "  Linia %d: ZLAMANA - konczy sie na: [%s]\n", NR, (length(line)>40 ? "..." substr(line,length(line)-39,40) : line)
+        found++
+        prev_broken = 1
+    } else {
+        if (prev_broken == 1) {
+            printf "           ^ powyzsze to kontynuacja od linii %d\n", NR-1
+        }
+        prev_broken = 0
+    }
+}
+END {
+    if (found == 0) print "  OK: Wszystkie linie koncza sie na |"
+    else printf "\n  UWAGA: %d zlaman linii\n", found
+}
+' "$PLIK"
