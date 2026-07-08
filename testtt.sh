562fde9d0a41449da11df4fd8c0143e9f70b5f75
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-07-08 18:22:21 +0200

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,13 +1,1 @@
-SELECT 
-    OBJECT_NAME(s.object_id) AS TableName,
-    i.name AS IndexName,
-    s.user_seeks, s.user_scans, s.user_lookups, s.last_user_seek
-FROM sys.dm_db_index_usage_stats s
-JOIN sys.indexes i ON s.object_id = i.object_id AND s.index_id = i.index_id
-WHERE s.object_id = OBJECT_ID('dbo.JobsSFTR');
-
-
-SELECT 
-    i.name, ips.avg_fragmentation_in_percent, ips.page_count
-FROM sys.dm_db_index_physical_stats(DB_ID(), OBJECT_ID('dbo.JobsSFTR'), NULL, NULL, 'LIMITED') ips
-JOIN sys.indexes i ON ips.object_id = i.object_id AND ips.index_id = i.index_id;
+java -cp /app/apache-tomcat-9.0.112/lib/mazovia-charset.jar -e "System.out.println(java.nio.charset.Charset.isSupported(\"X-Mazovia\"))"
