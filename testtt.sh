9e799e5b8354b0db98ac078de64f8a1f6f927b3f
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-07-20 13:58:23 +0200

    Update testtt.sh

--- testtt.sh
+++ testtt.sh
@@ -1,12 +1,4 @@
-curl -X POST "" \
-  -H "Content-Type: application/json" \
-  -d '{
-    "requestDate": "",
-    "data": [
-      {
-        "nip": "",
-        "nrb": "",
-        "id": "TEST_"
-      }
-    ]
-  }'
+SELECT MIN(a.id_wyc_trans + 1) 
+FROM wyc_trans a 
+WHERE NOT EXISTS (SELECT 1 FROM wyc_trans b WHERE b.id_wyc_trans = a.id_wyc_trans + 1)
+AND a.id_wyc_trans < 81128992;
