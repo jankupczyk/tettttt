5c3e5cea2f75a24146f19be10fda631f9ed0531b
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-07-21 09:27:31 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,86 +1,24 @@
-USE MIFIR;
-SELECT i.name AS index_name, i.type_desc,
-    STUFF((SELECT ', ' + c.name
-        FROM sys.index_columns ic
-        JOIN sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
-        WHERE ic.object_id = i.object_id AND ic.index_id = i.index_id AND ic.is_included_column = 0
-        ORDER BY ic.key_ordinal
-        FOR XML PATH('')), 1, 2, '') AS key_columns
-FROM sys.indexes i
-WHERE i.object_id = OBJECT_ID('dbo.JobsSFTR');
+-- 1. Wybierz "młodszy" rekord z każdej pary duplikatów (do przenumerowania)
+CREATE TABLE tmp_do_przenumerowania (
+  old_id INTEGER,
+  rowid_val INTEGER,  -- albo unikalny identyfikator fizycznego wiersza
+  new_id INTEGER
+);
 
-SELECT 
-    p.rows AS row_count,
-    SUM(a.total_pages) * 8 / 1024 AS total_MB,
-    ips.avg_fragmentation_in_percent
-FROM sys.partitions p
-JOIN sys.allocation_units a ON p.partition_id = a.container_id
-JOIN sys.dm_db_index_physical_stats(DB_ID(), OBJECT_ID('dbo.JobsSFTR'), NULL, NULL, 'LIMITED') ips
-    ON p.object_id = ips.object_id AND p.index_id = ips.index_id
-WHERE p.object_id = OBJECT_ID('dbo.JobsSFTR')
-GROUP BY p.rows, ips.avg_fragmentation_in_percent;
+-- 2. Wstaw kandydatów do przenumerowania z numeracją sekwencyjną
+INSERT INTO tmp_do_przenumerowania (old_id, rowid_val)
+SELECT id_wyc_trans, rowid
+FROM wyc_trans t
+WHERE id_wyc_trans IN (SELECT id_wyc_trans FROM wyc_trans GROUP BY id_wyc_trans HAVING COUNT(*) > 1)
+AND data_wal = (SELECT MAX(data_wal) FROM wyc_trans t2 WHERE t2.id_wyc_trans = t.id_wyc_trans); 
+-- ^ bierze "młodszy" wiersz z pary po dacie - dostosuj kryterium do ustalonej zasady
 
-SELECT 
-    migs.avg_total_user_cost * migs.avg_user_impact * (migs.user_seeks + migs.user_scans) AS improvement_measure,
-    mid.equality_columns, mid.inequality_columns, mid.included_columns
-FROM sys.dm_db_missing_index_groups mig
-JOIN sys.dm_db_missing_index_group_stats migs ON migs.group_handle = mig.index_group_handle
-JOIN sys.dm_db_missing_index_details mid ON mig.index_handle = mid.index_handle
-WHERE mid.statement = '[MIFIR].[dbo].[JobsSFTR]'
-ORDER BY improvement_measure DESC;
+-- 3. Nadaj nowe ID sekwencyjnie zaczynając od bezpiecznego punktu
+UPDATE tmp_do_przenumerowania 
+SET new_id = 85000001 + (SELECT COUNT(*) FROM tmp_do_przenumerowania t2 WHERE t2.rowid_val <= tmp_do_przenumerowania.rowid_val) - 1;
+-- to może być wolne przy 12 mln - lepiej zrobić przez unload/load z numeracją w skrypcie zewnętrznym (perl/python/awk)
 
-SELECT TOP 3
-    qs.execution_count, qs.total_logical_reads,
-    st.text AS full_query_text,
-    qp.query_plan
-FROM sys.dm_exec_query_stats qs
-CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) st
-CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
-WHERE st.text LIKE '%bEMailsSent%'
-ORDER BY qs.total_logical_reads DESC;
-
-
-USE MIFIR;
-CREATE NONCLUSTERED INDEX IX_JobsSFTR_JobType_ExecuteDateTime
-ON dbo.JobsSFTR (JobType, ExecuteDateTime)
-INCLUDE (Id, bEMailsSent, FileModifiedOn, HashCode, ConnectionString,
-         ErrorDescription, FileName, FinishedOn, FormatId, LinesCount,
-         EntriesCount, InsertDate, ProcessCode, ProcessId, RequestedBy,
-         StartedOn, Status, RecordsCount, SystemId, EntriesJobId)
-WITH (MAXDOP = 4, SORT_IN_TEMPDB = ON);
-
-
-SELECT YEAR(ExecuteDateTime) AS rok, MONTH(ExecuteDateTime) AS miesiac, COUNT(*) AS liczba_wierszy
-FROM MIFIR.dbo.JobsSFTR
-GROUP BY YEAR(ExecuteDateTime), MONTH(ExecuteDateTime)
-ORDER BY rok, miesiac;
-
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 0 -PropertyType DWORD -Force
-
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoAutorun" -Value 1 -PropertyType DWORD -Force
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DontDisplayLastUserName" -Value 1 -PropertyType DWORD -Force
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LogonType" -Value 0 -PropertyType DWORD -Force
-
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "NoInternetOpenWith" -Value 1 -PropertyType DWORD -Force
-
-Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
-Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
-Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer"
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- 4. Wykonaj UPDATE hurtowo używając rowid jako klucza fizycznego
+UPDATE wyc_trans 
+SET id_wyc_trans = (SELECT new_id FROM tmp_do_przenumerowania WHERE rowid_val = wyc_trans.rowid)
+WHERE rowid IN (SELECT rowid_val FROM tmp_do_przenumerowania);
