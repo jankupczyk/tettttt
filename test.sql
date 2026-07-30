99fed155b5504e46f0325ed8878ad7e8fd2641d4
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2026-07-30 10:00:50 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -5,3 +5,10 @@ grep -E "^HostKeyAlgorithms|^PubkeyAcceptedKeyTypes" sshd_config
 
 
 /usr/sbin/sshd -t -f /etc/ssh/sshd_config
+
+sed 's/,\{0,1\}rsa-sha2-512-cert-v01@openssh\.com//g;
+s/,\{0,1\}rsa-sha2-256-cert-v01@openssh\.com//g;
+s/,\{0,1\}rsa-sha2-512//g;
+s/,\{0,1\}rsa-sha2-256//g;
+s/,\{0,1\}ssh-rsa-cert-v01@openssh\.com//g;
+s/,\{0,1\}ssh-rsa//g' sshd_config > sshd_config.new
